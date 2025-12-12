99acc02477f69d64e965f32f2bb555240f830aad
CYRTRUS <totae9949@gmail.com>
2025-12-12 21:09:59 +0700

    Give up and neutralize everything (T-T)

--- components/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Components package for Crypto Dashboard
-"""
-from .ticker import CryptoTicker
-from .apps import MultiTickerApp, ToggleableTickerApp, fetch_initial_data
-
-__all__ = ["CryptoTicker", "MultiTickerApp",
-           "ToggleableTickerApp", "fetch_initial_data"]
-__version__ = "1.0.0"
